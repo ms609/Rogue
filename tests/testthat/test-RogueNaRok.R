0bbe60e434ec1adba2f14ee4d06796f7c882b4cb
Martin Smith <martins@gmail.com>
2021-06-14 17:05:17 +0100

    0 means return is ok

--- tests/testthat/test-RogueNaRok.R
+++ tests/testthat/test-RogueNaRok.R
@@ -1,6 +1,7 @@
 test_that("RogueNaRok() doesn't explode", {
   bootTrees <- system.file('example/150.bs', package = 'RogueTaxa')
   treeFile <- system.file('example/150.tr', package = 'RogueTaxa')
-  RogueNaRok(bootTrees = bootTrees, treeFile = treeFile)
+  expect_equal(0, RogueNaRok(bootTrees = bootTrees, treeFile = treeFile,
+                             run_id = 1))
   
 })
