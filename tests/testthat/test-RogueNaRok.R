03d88eeb17cbac92daf893182f837e96eb741df6
Martin Smith <martins@gmail.com>
2021-06-15 21:42:02 +0100

    Test 1

--- tests/testthat/test-RogueNaRok.R
+++ tests/testthat/test-RogueNaRok.R
@@ -1,9 +1,22 @@
+Delete <- function (f) if (file.exists(f)) file.remove(f)
 test_that("RogueNaRok() doesn't explode", {
   bootTrees <- system.file('example/150.bs', package = 'RogueTaxa')
   treeFile <- system.file('example/150.tr', package = 'RogueTaxa')
-  expect_equal(0, RogueNaRok(bootTrees = bootTrees, treeFile = treeFile,
-                             run_id = 1))
+  Delete('RogueNaRokR_droppedRogues.tmp')
+  Delete('RogueNaRokR_info.tmp')
+  expect_equal(0, C_RogueNaRok(bootTrees = bootTrees,# treeFile = treeFile,
+                               dropsetSize = 1,
+                               runId = 'tmp'))
   
+  dims <- dim(read.table('RogueNaRokR_droppedRogues.tmp', header = TRUE))
+  expect_lt(2, dims[1])
+  expect_equal(5, dims[2])
+  # if run_id exists, won't run.
+  Delete('RogueNaRokR_droppedRogues.tmp')
+  Delete('RogueNaRokR_info.tmp')
+  
+})
+
 test_that("Rogues found", {
   library("TreeTools", warn.conflicts = FALSE, quietly = TRUE)
   trees <- AddTipEverywhere(BalancedTree(8), 'Rogue')
