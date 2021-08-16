142c52d412d7bd4e44d3a7075de3318b45347e47
Martin Smith <martins@gmail.com>
2021-08-16 09:38:30 +0100

    Don't fail on possible leak (from other packages)

--- memcheck/examples.R
+++ memcheck/examples.R
@@ -1,5 +1,3 @@
-# Code to be run with
+# Script to be run with
 #   R -d "valgrind --tool=memcheck --leak-check=full --error-exitcode=1" --vanilla < tests/thisfile.R
-# First build and install the package.
-library('Rogue')
 devtools::run_examples()
