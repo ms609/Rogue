76c00552ee632d88b54dca8e46c5d1a8e652619a
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2022-09-26 09:51:14 +0100

    All notes now in Cran Comments

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,3 @@
 .onUnload <- function(libpath) {
   library.dynam.unload("Rogue", libpath)
 }
-
-# Additional tests:
-#
-# spell_check()
-#
-# run_examples()
-#
-# devtools::check_win_devel(quiet = TRUE); rhub::check_for_cran()
-# Check valgrind results on Github Actions
-# revdepcheck::revdep_check()
-#
-# codemetar::write_codemeta()
